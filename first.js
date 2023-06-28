50f0ce8d98d605660f07c1fd6218c9c6efc85b99
Oculux314 <nate.williamson.nz@gmail.com>
2023-06-28 14:23:41 +1200

    Intro to dates

--- first.js
+++ first.js
@@ -2,3 +2,15 @@ console.log("Hello world!");
 console.log("1");
 console.log(1);
 console.log(true);
+
+// Get current date
+now = new Date();
+console.log(now);
+
+// Unix 0 (1st January 1970)
+unix0 = new Date(0);
+console.log(unix0);
+
+// Random date between 1970 and now
+randomDate = new Date(Math.random() * now.getTime());
+console.log(randomDate);
