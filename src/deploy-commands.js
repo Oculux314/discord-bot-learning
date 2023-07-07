c0134aa3c4b1e9578274ab336dd22405a6b4656f
Oculux314 <nate.williamson.nz@gmail.com>
2023-07-07 16:54:30 +1200

    Fixed deploy script only recognising commands in subfolders

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -14,10 +14,19 @@ const commands = [];
 const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
-for (const folder of commandFolders) {
+for (const element of commandFolders) {
   // Grab all the command files from the commands directory you created earlier
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+  let commandFiles;
+  let commandsPath;
+  if (element.endsWith(".js")) {
+    // File
+    commandsPath = foldersPath;
+    commandFiles = [element];
+  } else {
+    // Folder
+    commandsPath = path.join(foldersPath, element);
+    commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+  }
 
   // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
   for (const file of commandFiles) {
