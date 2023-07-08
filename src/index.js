c8ea34d29be2aad85d54e73bf5f613d1e8c5b724
Oculux314 <nate.williamson.nz@gmail.com>
2023-07-08 13:53:05 +1200

    First replies

--- src/index.js
+++ src/index.js
@@ -6,15 +6,40 @@ const {TOKEN} = process.env;
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    // GatewayIntentBits.GuildMembers,
-    // GatewayIntentBits.GuildMessages,
-    // GatewayIntentBits.DirectMessages,
-    // GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
   ],
 });
 
 client.once(Events.ClientReady, c => {
   console.log(`Client ready! Logged in as ${c.user.tag}.`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+  const path = `./commands/${interaction.commandName}.js`;
+  const command = require(path);
+
+  try {
+    command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    const errorReply = {content: "There was an error while executing this command!", ephemeral: true};
+
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp(errorReply);
+    } else {
+      await interaction.reply(errorReply);
+    }
+  }
+});
+
+client.on(Events.MessageCreate, msg => {
+  if (msg.author.bot) return;
+  console.log(`${msg.member.displayName}: ${msg.content}`);
+  msg.reply({content: `Greetings,\nI am ${msg.member.displayName}.`}); // Chara
 });
 
 client.login(TOKEN);
